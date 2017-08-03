e2b36b6cc757e64628390845212e4052ae8682bd
Yuanjun Zhong <zhuimengzyj@gmail.com>
2017-08-03 16:26:33 +0800

    将url,dist选项移至fetch命令中
    Changes to be committed:
    	modified:   index.js

--- index.js
+++ index.js
@@ -3,23 +3,25 @@ var yargs = require('yargs')
     ;
 
 yargs.options({
+});
+
+yargs.command('fetch', '下载漫画', {
+    'dist': {
+        alias: 'd',
+        describe: '保存位置(默认{当前进程所在目录}/download)'
+    },
     'url': {
         alias: 'u',
         describe: '漫画介绍页url',
         demandOption: true
-    },
-    'dist': {
-        alias: 'd',
-        describe: '保存位置(默认{当前进程所在目录}/download)'
     }
-});
-
-yargs.command('fetch', '下载漫画', {
-
 }, function (argv) {
     var url = argv['url']
         , dist = argv['dist'] || path.join(process.cwd(), 'download')
         ;
+    /**
+     * @todo 处理该命令
+     */
     console.log(url, dist);
 });
 
