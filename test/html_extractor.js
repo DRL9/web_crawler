72a03815ae4578b55bd9c4f05baeb1dd8cd1359a
Yuanjun Zhong <zhuimengzyj@gmail.com>
2017-08-15 15:12:16 +0800

    add html_extractor模块,提取页面相关信息（未完成）
    Changes to be committed:
    	new file:   lib/html_extractor.js
    	new file:   test/html_extractor.js

---
+++ test/html_extractor.js
@@ -0,0 +1,51 @@
+const assert = require('chai').assert
+    , htmlExtractor = require('../lib/html_extractor')
+    ;
+
+describe('html_extractor', () => {
+    describe('.extractComicsContent(html)', () => {
+        it('should return ContentInfo when pass right html', () => {
+            var html = '';
+            var result = htmlExtractor.extractComicsContent(html);
+
+            assert.property(result, 'nextPageUrl');
+            assert.property(result, 'curComicsUrl');
+            assert.property(result, 'curPageNum');
+            assert.property(result, 'totalPageNum');
+
+            assert.isString(result.nextPageUrl);
+            assert.isString(result.curComicsUrl);
+            assert.isNumber(result.curPageNum);
+            assert.isNumber(result.totalPageNum);
+        });
+
+        it('should throw error when pass wrong html', () => {
+            assert.throw(() => {
+                htmlExtractor.extractComicsContent('wronghtml')
+            });
+        });
+    });
+
+    describe('.extractComicsIntroduce(html)', () => {
+        it('should return IntroduceInfo when pass right html', () => {
+            var html = '';
+            var result = htmlExtractor.extractComicsIntroduce(html);
+
+            assert.property(result, 'title');
+            assert.property(result, 'firstPageUrl');
+            assert.property(result, 'totalPageNum');
+            assert.property(result, 'tags');
+
+            assert.isString(result.title);
+            assert.isSealed(result.firstPageUrl);
+            assert.isNumber(result.totalPageNum);
+            assert.isArray(result.tags);
+        });
+
+        it('should throw error when pass wrong html', () => {
+            assert.throw(() => {
+                htmlExtractor.extractComicsIntroduce('wronghtml')
+            });
+        });
+    });
+});
