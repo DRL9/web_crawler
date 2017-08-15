33552e2d56f65714ffc147823a39c0aca8d657d8
Yuanjun Zhong <zhuimengzyj@gmail.com>
2017-08-15 19:26:42 +0800

    添加crawler模块
    Changes to be committed:
    	new file:   lib/crawler.js
    	new file:   test/crawler.js

---
+++ test/crawler.js
@@ -0,0 +1,35 @@
+const chai = require('chai')
+    , chaiAsPromised = require('chai-as-promised')
+    , crawler = require('../lib/crawler')
+    ;
+
+chai.should();
+chai.use(chaiAsPromised);
+
+describe('crawler', () => {
+    describe('.crawl(url)', function () {
+        this.timeout(4000);
+
+        it('should be rejected because of timeout', () => {
+            return crawler.crawl('http://localhost:3001/timeout?time=2000', 2000).should.be.rejected;
+        });
+
+        it('should be rejected because of wrong resp code', () => {
+            return crawler.crawl('http://localhost:3001/wrongresp?code=404').should.be.rejected;
+        });
+
+        it('should be resolved', () => {
+            return crawler.crawl('http://localhost:3001/right?content=hello').should.eventually.equal('hello');
+        });
+    });
+
+    describe('.crawlWithRetry(url)', () => {
+        it('should be resolved', () => {
+            return crawler.crawlWithRetry('http://localhost:3001/right?content=hello').should.eventually.equal('hello');
+        });
+
+        it('should be rejected', () => {
+            return crawler.crawlWithRetry('http://localhost:3001/wrongresp?code=404', 2).should.be.rejectedWith(Error, /第3次失败/)
+        });
+    });
+});
